1fef3648c5e22c8c705a4aa578b718486b6cb781
Chau Chee Yang <ccy@sql.com.my>
2016-03-12 09:05:06 +0800

    How function passing makes our HTTP server work

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 var http = require("http");
 
-var server = http.createServer();
-server.listen(8888);
+function onRequest(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+};
+
+http.createServer(onRequest).listen(8888);
