00783ac52c76ac8ee0319320d22014ef8d72c8a9
Chau Chee Yang <ccy@sql.com.my>
2016-03-12 10:02:34 +0800

    Responding request handlers with non-blocking operations #2

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,11 +3,13 @@ var exec = require("child_process").exec;
 function start(response) {
   console.log("Request handler 'start' was called.");
 
-  exec("dir /s \\", function (error, stdout, stderr) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(stdout);
-    response.end();
-  });
+  exec("dir /s \\",
+    { timeout: 60000, maxBuffer: 20000*1024 },
+    function (error, stdout, stderr) {
+      response.writeHead(200, {"Content-Type": "text/plain"});
+      response.write(stdout);
+      response.end();
+    });
 }
 
 function upload(response) {
